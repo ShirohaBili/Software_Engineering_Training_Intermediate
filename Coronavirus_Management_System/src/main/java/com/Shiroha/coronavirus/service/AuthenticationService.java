08e105716bc33a1f5eb357e0df8e6b4f22317808
ShirohaBili <463059942@qq.com>
2023-01-10 15:39:26 +0800

    del

--- Coronavirus_Management_System/src/main/java/com/Shiroha/coronavirus/service/AuthenticationService.java
+++
@@ -1,9 +0,0 @@
-package com.Shiroha.coronavirus.service;
-
-import com.Shiroha.coronavirus.entity.Authentication;
-
-import java.util.List;
-
-public interface AuthenticationService {
-    public List<Authentication> findByUserId(int userId);
-}
